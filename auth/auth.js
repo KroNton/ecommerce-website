1a58cd05278521a4f6e00fd4d3192d1c4f551bf6
KroNton <kyrlosfekry@gmail.com>
2025-05-19 02:16:49 +0300

    implement registration and sign-in form validation with error handling

--- auth/auth.js
+++ auth/auth.js
@@ -1,6 +1,12 @@
 const container = document.getElementById('container');
 
 
+const register_form = document.getElementById('register-form');
+const signIn_form = document.getElementById('signin-form');
+
+
+
+// control Page Toggle
 function handleRegister() {
     container.classList.add("active");
 }
@@ -8,3 +14,70 @@ function handleLogin() {
     container.classList.remove("active");
 }
 
+register_form.addEventListener('submit', async (e) => {
+    // 
+    const name = signUpForm.querySelector('input[type="text"]').value;
+    const email = signUpForm.querySelector('input[type="email"]').value;
+    const password = signUpForm.querySelector('input[type="password"]').value;
+    const confirmPassword = signUpForm.querySelectorAll('input[type="password"]')[1].value;
+    let errors = []
+    errors = getSignupFormErrors(name.value, email.value, password.value, confirmPassword.value);
+
+    if (errors.length >= 0) {
+        // show error to UI
+
+        //prevent user to SignUP
+        e.preventDefault();
+    }
+    else {
+        // post data to DB
+
+    }
+
+})
+
+signIn_form.addEventListener('submit', async (e) => {
+    // e.preventDefault();
+    const email = signUpForm.querySelector('input[type="email"]').value;
+    const password = signUpForm.querySelector('input[type="password"]').value;
+    let errors = []
+    errors = getSignInFormErrors(email.value, password.value);
+
+    if (errors.length >= 0) {
+        // show error to UI
+
+        //prevent user to SignUP
+        e.preventDefault();
+    }
+    else {
+        // validate data in DB
+
+    }
+
+})
+
+function getSignupFormErrors(name, email, password, confirmPassword) {
+    let errors = [];
+
+    if (name === '' || name == null) {
+        errors.push('Name is Required.')
+    }
+
+    if (password !== confirmPassword) {
+
+
+    }
+
+    return errors
+}
+
+
+// Frontend validation
+
+
+
+
+
+function getSignInFormErrors() {
+
+}
